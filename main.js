cf7a09aedba94bd027d972111fc8b9700bb7bdea
Kangnos <rkdtlsgur66@gmail.com>
2020-11-25 23:01:22 +0900

    2020 11 25

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const app = express();
 const fs = require("fs");
 var compression = require('compression')
 app.use(compression())
+var WeatherFlexMainpage = require('./lib/main.js')
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-    fs.readFile("main.html", function(err, data){
-        res.end(data)
-    })
-})
-
+app.get('/', function (req, res) {
+    var mainpage = WeatherFlexMainpage.HTML();
+    res.send(mainpage)
+});
 
 app.listen(3000, function () {
     console.log("Example app is running")
