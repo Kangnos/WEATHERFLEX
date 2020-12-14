d22adb9e99bae62a15d06e608c82562c555688a9
Kangnos <rkdtlsgur66@gmail.com>
2020-12-14 23:00:10 +0900

    2020 12 14

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ const fs = require("fs");
 var compression = require('compression')
 app.use(compression())
 var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 var WeatherFlexMainpage = require('./lib/searchpage.js')
 var WeatherFlexWeatherpage = require("./lib/weatherpage.js");
+var Weatherflex_main_code = require("./public/main.js");
+const { request } = require('http');
+
 
 app.use(express.static('public'));
 
@@ -14,7 +21,8 @@ app.get('/', function (req, res) {
     res.send(weatherflex_homepage);
 });
 
-app.get("/search", function(req,res){
+app.get("/search/:id", function(req,res){
+    console.log(req.body.user.name)
     var weatherflex_weatherpage = WeatherFlexWeatherpage.HTML();
     res.send(weatherflex_weatherpage);
 })
--- public/main.js
+++ public/main.js
@@ -1,13 +1,13 @@
-var title = document.getElementById("title")
-var City = document.getElementById("City");
-var Temp = document.getElementById("Temp");
-var FeelingTemp = document.getElementById("FeelingTemp")
-var Weathernow = document.getElementById("weathernow")
-var min_max_temperature = document.getElementById("min_max_temperature")
-var humidity = document.getElementById("humidity")
-var windspeed = document.getElementById("windspeed")
+const { request } = require("express");
 
 function input_text() {
+    var City = document.getElementById("City");
+    var Temp = document.getElementById("Temp");
+    var FeelingTemp = document.getElementById("FeelingTemp")
+    var Weathernow = document.getElementById("weathernow")
+    var min_max_temperature = document.getElementById("min_max_temperature")
+    var humidity = document.getElementById("humidity")
+    var windspeed = document.getElementById("windspeed")
     var citynameinputText = document.getElementById("Weather_input").value;
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${citynameinputText}&appid=cbe7f1eb13ae670e2e99a200f1df5a94&units=metric&lang=kr`
