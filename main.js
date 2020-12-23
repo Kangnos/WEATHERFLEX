68662d17ec13f3a679d040112f9e7c1183d8c114
Kangnos <rkdtlsgur66@gmail.com>
2020-12-23 22:53:38 +0900

    2020 12 23

--- main.js
+++ main.js
@@ -7,21 +7,30 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+var request = require('request');
 var url = require('url');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 const Homepage = require("./lib/Homepage.js")
 const WeatherFlexMainpage  = require("./lib/weatherpage.js");
-const { request } = require('http');
 
 
 app.get('/', function (req, res) {
-    var Homepagetemplate = Homepage.HTML();
+    var city_name = req.query.placename;
+    var Homepagetemplate = Homepage.HTML(city_name);
     res.send(Homepagetemplate);
 });
 
 app.use("/search", function(req,res){
-    var city_name = req.query.placename.toLowerCase();
+    var city_name = req.query.placename;
+    console.log(city_name)
+    request('https://api.openweathermap.org/data/2.5/weather?q='+ city_name + '&appid=cbe7f1eb13ae670e2e99a200f1df5a94&units=metric&lang=kr',function(error, response, body){
+        if(!error&&response.statusCode==200){
+            console.log(body);
+            const json = JSON.parse(body);
+            console.log(json.weather)
+        }
+    });
     var WeatherFlexWeatherpage = WeatherFlexMainpage.HTML(city_name);
     res.send(WeatherFlexWeatherpage);
 })
